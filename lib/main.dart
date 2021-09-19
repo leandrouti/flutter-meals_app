56bff6b3a8a64f22d90b3a477d5fc821344e8543
Leandro T. Utiyama <lutiyama@gmail.com>
2021-09-19 10:23:01 +0900

    [add] meal details screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         routes: {
           CategoryMealsScreen.routeName: (context) =>
               const CategoryMealsScreen(),
+          MealDetailScreen.routeName: (context) => const MealDetailScreen(),
         });
   }
 }
