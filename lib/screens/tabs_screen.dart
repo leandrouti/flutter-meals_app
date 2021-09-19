0cee340118eda64029535367c4b5cee22473cd78
Leandro T. Utiyama <lutiyama@gmail.com>
2021-09-20 08:15:30 +0900

    [add] drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
 import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
@@ -35,6 +36,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedpageIndex]['title']),
       ),
+      drawer: const MainDrawer(),
       body: _pages[_selectedpageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Theme.of(context).primaryColor,
