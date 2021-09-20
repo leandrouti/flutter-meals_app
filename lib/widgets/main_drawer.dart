f12fe65707c35ce52093f4e7ad30773fde6ecf38
Leandro T. Utiyama <lutiyama@gmail.com>
2021-09-20 12:14:34 +0900

    [change] drawer push named

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -48,14 +48,18 @@ class MainDrawer extends StatelessWidget {
             'Meals',
             Icons.restaurant,
             () {
-              Navigator.of(context).pushNamed('/');
+              Navigator.of(context).pushReplacementNamed(
+                '/',
+              );
             },
           ),
           _buildListTile(
             'Filters',
             Icons.settings,
             () {
-              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+              Navigator.of(context).pushReplacementNamed(
+                FiltersScreen.routeName,
+              );
             },
           ),
         ],
